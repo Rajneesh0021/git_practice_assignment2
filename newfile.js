0ab55b0ecf84388b7ceb78461ea7c0d1e98ce5ad
rajneesh <thedreamerone0021@gmail.com>
2022-09-15 18:36:05 +0530

    new assignment

---
+++ newfile.js
@@ -0,0 +1,21 @@
+//palindrome problem
+let u='naman';
+let bag='';
+for(i=u.length-1;i>=0;i--){
+   bag=bag+u[i]}
+   if(u==bag){console.log('palindrome')
+}else{console.log(bag)}
+
+ //prime number problem
+ let flag = 'prime'
+let num =12;
+for(i=2;i<num;i++){
+if(num%i==0){
+flag='not prime';
+break;
+}
+}
+if(flag){
+ console.log(num, flag);
+}
+else {console.log(num, flag)}
